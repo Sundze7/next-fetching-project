497a90ac4fd931beb7a1a335504f32b94b74e262
Sundze <keyonmwesundze@mail.com>
2024-09-09 07:49:20 +0100

    incremental static generation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
+import fs from "fs/promises";
+import Link from "next/link";
+import path from "path";
+
 function HomePage(props) {
-    return (
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
-    );
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link key={product.id} href={`/${product.id}`}>
+            {product.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  // special fields on the getStaticProps( )
+  if (!data) {
+    return {
+      redirect: { destination: "/some-route" },
+    };
   }
-  
-  export default HomePage;
+
+  if (data.products.length == 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
